6d218ef3cea51d487aed6d44f5fbcf60288865d2
CompuComRDF <131194544+CompuComRDF@users.noreply.github.com>
2026-07-03 13:44:50 -0400

    Update getConnectConfig.js
    Updated SSO login URL's

--- src/config/getConnectConfig.js
+++ src/config/getConnectConfig.js
@@ -1,7 +1,12 @@
+//const SSO_LOGIN_URLS_BY_INSTANCE = {
+//    "uatrdf-genesysdr": "https://us-east-1.console.aws.amazon.com/connect/federate/b976f223-5ca9-4445-9a6f-d890d8dee0cd", 
+//    "prodrdf-genesysdr": "https://us-east-1.console.aws.amazon.com/connect/federate/894c5ab7-3400-4001-b3d9-797f65f10ecf" 
+//    };
+
 const SSO_LOGIN_URLS_BY_INSTANCE = {
-    "uatrdf-genesysdr": "https://us-east-1.console.aws.amazon.com/connect/federate/b976f223-5ca9-4445-9a6f-d890d8dee0cd", 
-    "prodrdf-genesysdr": "https://us-east-1.console.aws.amazon.com/connect/federate/894c5ab7-3400-4001-b3d9-797f65f10ecf" 
-    };
+"uatrdf-genesysdr": "https://account.activedirectory.windowsazure.com/applications/signin/3d1e37fc-6119-4055-921a-6fae7e40c2f6?tenantId=fa462fb8-0560-430e-8331-6a29355f95ca",
+"prodrdf-genesysdr": "https://account.activedirectory.windowsazure.com/applications/signin/99a68e1b-b1f9-4e73-b185-92a5f0567646?tenantId=fa462fb8-0560-430e-8331-6a29355f95ca"
+};
 
 export function getConnectConfig() {
     const params = new URLSearchParams(window.location.search);
